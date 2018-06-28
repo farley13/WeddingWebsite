71535126c559a223b1589b6d6b40446a2cbf995c
Adam Farley <farley13@gmail.com>
2018-06-27 23:22:11 -0400

    RSVP Functions - loading from sheet works

--- js/adam_and_sonia.js
+++ js/adam_and_sonia.js
@@ -137,6 +137,190 @@ var wedding = (function () {
        });
    }
 
+   var search_for_rsvp = function() {
+
+       var firstName=$("#firstName")[0].value; 
+       var firstNameEscaped = encodeURIComponent(firstName).trim();
+
+       var lastName=$("#lastName")[0].value; 
+       var lastNameEscaped = encodeURIComponent(lastName).trim();
+
+       if (firstName === "" || lastName === "")
+       {
+	   $("#search_message").html("Error: Please fill out first and last name before searching.");
+	   return;
+       }
+
+       $("#search_message").html("Searching for RSVP...")
+       $("#weddingPartyArea").html("");
+       $("#emailAndMessage").attr("style", "display:none");
+
+       // fire off the request to our webapp
+       request = $.ajax({
+           url: "https://script.google.com/macros/s/AKfycbxwLF8OBywjaWGZ59qENIVM4QY4wvcVQojQBBQ4YiQIxwi8N4E/exec?isRSVPQuery=true&firstName="+firstNameEscaped+"&lastName="+lastNameEscaped,
+	   dataType: 'json'
+       });
+       
+       // Callback handler that will be called on success
+       request.done(function (response, textStatus, jqXHR){
+	   
+
+//	   $("#search_message").html("It looks like your browser may be a bit out of date. You many try using a newer computer or reach out to Adam and Sonia for help!")
+
+	   $("#weddingPartyArea").html(""); // make sure if we finish multiple times, only one is present on page
+	   var message = "Your wedding party is provided below: ";
+
+	   for (i in response.rows) {
+	       row = response.rows[i];
+
+	       var templateClone;
+	       if (row.invitedFriday == "1" && row.invitedSaturday == "1") {
+		   templateClone = $("#bothDaysTemplate").clone();
+	       } else if (row.invitedSaturday == "1") {
+		   templateClone = $("#saturdayOnlyTemplate").clone();
+	       } else {
+		   templateClone = $("#fridayOnlyTemplate").clone();
+	       }
+	       $(templateClone).attr("id", "guest_" + i);
+	       $(templateClone).attr("style", "");
+
+	       if(row.unknownName == "1") {
+		   $(templateClone).find("#inputName").attr("readonly", false);
+		   $(templateClone).find("#inputName").val("");
+	       }
+	       else {
+		   var name = row.firstName + " " + row.lastName;
+		   $(templateClone).find("#inputName").val(name);
+	       }
+
+	       $(templateClone).find("#attendeeLabel").html("Attendee " + (Number(i) + 1));
+
+	       if (row.attendingFriday == "0") {
+		   $(templateClone).find("#attendingFridayInput").val("I will not attend");
+	       } else {
+		   $(templateClone).find("#attendingFridayInput").val("I will attend");
+	       }
+
+	       if (row.attendingSaturday == "0") {
+		   $(templateClone).find("#attendingSaturdayInput").val("I will not attend");
+	       } else {
+		   $(templateClone).find("#attendingSaturdayInput").val("I will attend");
+	       }	       
+
+	       var foodClone;
+	       if (row.child == "1") {
+		   foodClone = $("#foodOptionsChildTemplate").clone();
+	       }
+	       else {
+		   foodClone = $("#foodOptionsAdultTemplate").clone();
+	       }
+	       $(foodClone).attr("id", "food_" + i);
+	       $(foodClone).attr("style", "");
+
+	       $(foodClone).find("#inputFirstCourse").val(row.firstCourse);
+	       $(foodClone).find("#inputMainCourse").val(row.mainCourse);
+	       $(foodClone).find("#inputDietaryRestrictions").val(row.dietaryRestrictions);
+
+	       $(templateClone).find("#foodOptions").append(foodClone);
+	       $("#weddingPartyArea").append(templateClone);
+	       
+	       if (row.email != "") {
+		   $("#inputEmailAddress").val(row.email);
+	       }
+	       if (row.message != "") {
+		   $("#inputMessage").val(row.message);
+	       }
+	       
+	   }
+
+	   $("#emailAndMessage").attr("style", "");
+
+	   $("#search_message").html(message);
+	   
+	   
+       });
+
+       // Callback handler that will be called on failure
+       request.fail(function (jqXHR, textStatus, errorThrown){
+           // Log the error to the console
+           console.error(
+               "The following error occurred: "+
+		   textStatus, errorThrown
+           );
+	   if (errorThrown != ""){
+	       $("#search_message").html("Something didn't quite work this time. Please try again later!")
+	   }
+	   else { // if no one can tell us what the error was... maybe there wasn't any? Older Ipads have this issue
+	       $("#search_message").html("It looks like your browser may be a bit out of date. You many try using a newer computer or reach out to Adam and Sonia for help!")
+	   }
+       });
+
+       // Callback handler that will be called regardless
+       // if the request failed or succeeded
+       request.always(function () {
+           // Reenable the inputs
+//           $inputs.prop("disabled", false);
+       });
+   }
+
+   var update_rsvp = function() {
+
+       var email=$("#email")[0].value; 
+       var emailEscaped = encodeURIComponent(email);
+
+       var firstName=$("#firstName")[0].value; 
+       var firstNameEscaped = encodeURIComponent(firstName);
+
+       var lastName=$("#lastName")[0].value; 
+       var lastNameEscaped = encodeURIComponent(lastName);
+
+       if (email === "" || firstName === "" || lastName === "")
+       {
+	   $("#subscribe_message").html("Error: Please fill out email, first and last name before submitting.");
+	   return;
+       }
+
+       $("#subscribe_message").html("Subscribing...")
+
+       // fire off the request to our webapp
+       request = $.ajax({
+//           url: "https://script.google.com/macros/s/AKfycbxwLF8OBywjaWGZ59qENIVM4QY4wvcVQojQBBQ4YiQIxwi8N4E/exec?name="+ emailEscaped +"&amp;comment=None&amp;firstName=Adam&amp;lastName=Farley",
+           url: "https://script.google.com/macros/s/AKfycbxwLF8OBywjaWGZ59qENIVM4QY4wvcVQojQBBQ4YiQIxwi8N4E/exec?name="+ emailEscaped +"&comment=None&firstName="+firstNameEscaped+"&lastName="+lastNameEscaped,
+	   dataType: 'json'
+	   //            data: serializedData
+//           data: "name=Adam&comment=None"
+       });
+       
+       // Callback handler that will be called on success
+       request.done(function (response, textStatus, jqXHR){
+           // Log a message to the console
+           $("#subscribe_message").html("You have been subscribed! We will email you when there are updates or new information.")
+       });
+
+       // Callback handler that will be called on failure
+       request.fail(function (jqXHR, textStatus, errorThrown){
+           // Log the error to the console
+           console.error(
+               "The following error occurred: "+
+		   textStatus, errorThrown
+           );
+	   if (errorThrown != ""){
+	       $("#subscribe_message").html("Something didn't quite work this time. Please try again later!")
+	   }
+	   else { // if no one can tell us what the error was... maybe there wasn't any? Older Ipads have this issue
+	       $("#subscribe_message").html("You have been subscribed! We will email you when there are updates or new information.")
+	   }
+       });
+
+       // Callback handler that will be called regardless
+       // if the request failed or succeeded
+       request.always(function () {
+           // Reenable the inputs
+//           $inputs.prop("disabled", false);
+       });
+   }
+
+
   // Explicitly reveal public pointers to the private functions 
   // that we want to reveal publicly
 
@@ -145,6 +329,8 @@ var wedding = (function () {
     work_around_broken_hashtags: work_around_broken_hashtags,
     launch_photo_gallery: launch_photo_gallery,
     launch_subscribe_dialog: launch_subscribe_dialog,
+    search_for_rsvp: search_for_rsvp,
+    update_rsvp: update_rsvp,
     bind_swiping: bind_swiping,
     failing: failing
   }
@@ -157,6 +343,12 @@ $( document ).ready(function() {
     wedding.work_around_broken_hashtags();
     wedding.bind_swiping();
     launchPhotoGallery();
+    // make sure we preserve newlines in text area
+    $.valHooks.textarea = {
+	get: function( elem ) {
+	    return elem.value.replace( /\r?\n/g, "\r\n" );
+	}
+    };
     $.ajaxSetup({ cache: false }); // or iPhones don't get fresh data
 });
 
